b8d5a4befc94f8b5b835da6d52ad1e129f5ab4f4
Jsegel <segeljosh@gmail.com>
2019-12-15 22:29:52 -0600

    added function that adds possible options to array

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var specialChar = ['!', '"', '#', '$', '%', '&', '"', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', '<', '>', '=', '?', '@', '[', ']', "\ ", '^', '_', '`', '{', '}', '|', '~'];
 var numChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x', 'y', 'z'];
-var upperchar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 var isSpecial;
 var isNum;
@@ -31,23 +31,40 @@ function buttonClick() {
     if (length < 8 || isNaN(length)) {
         alert ('Please Enter Number Between 8 and 128')
     }
+    optionBuilder();
+}
+function optionBuilder() {
+    if (isLower || isUpper || isNum || isSpecial) {
+        if (isLower) {
+            possibleOptions = possibleOptions.concat(lowerChar);
+        }
+        if (isUpper) {
+            possibleOptions = possibleOptions.concat(upperChar);
+        }
+        if (isNum) {
+            possibleOptions = possibleOptions.concat(numChar);
+        }
+        if (isSpecial) {
+            possibleOptions = possibleOptions.concat(specialChar);
+        }
+    }
 }
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
-}
+//   copyBtn.removeAttribute("disabled");
+//   copyBtn.focus();
+// }
 
-function copyToClipboard() {
-  // BONUS 
-}
+// function copyToClipboard() {
+//   // BONUS 
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
